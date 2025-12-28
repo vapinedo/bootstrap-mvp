4d763a7ec7fedec45e4251c0976be10ddf4bcc54
Victor Pinedo <victorpinedomartinez@gmail.com>
2025-12-28 15:46:33 -0500

    Renombrar features folder (#16)

--- apps/frontend/src/features/dashboard/index.js
+++
@@ -1,8 +0,0 @@
-export { DashboardRouter } from '@features/dashboard/DashboardRouter';
-export { DashboardPage } from '@features/dashboard/pages/DashboardPage';
-export { RecentOrders } from '@features/dashboard/components/ecommerce/RecentOrders';
-export { MonthlyTarget } from '@features/dashboard/components/ecommerce/MonthlyTarget';
-export { StatisticsChart } from '@features/dashboard/components/ecommerce/StatisticsChart';
-export { DemographicCard } from '@features/dashboard/components/ecommerce/DemographicCard';
-export { EcommerceMetrics } from '@features/dashboard/components/ecommerce/EcommerceMetrics';
-export { MonthlySalesChart } from '@features/dashboard/components/ecommerce/MonthlySalesChart';
--- apps/frontend/src/modules/dashboard/DashboardRouter.jsx
+++ apps/frontend/src/modules/dashboard/DashboardRouter.jsx
@@ -1,6 +1,6 @@
 
 import { Outlet } from 'react-router-dom';
-import { DashboardPage } from '@features/dashboard/pages/DashboardPage';
+import { DashboardPage } from '@modules/dashboard/pages';
 
 export const DashboardRouter = () => {
   return (
