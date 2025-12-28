06d8bfccc6c9363b080d5b62b568173f59503e46
Victor Pinedo <victorpinedomartinez@gmail.com>
2025-12-28 14:47:45 -0500

    ajustar rutas

--- apps/frontend/src/features/dashboard/DashboardRouter.jsx
+++ apps/frontend/src/features/dashboard/DashboardRouter.jsx
@@ -1,14 +1,12 @@
 
-import { Routes, Route } from 'react-router-dom';
-import { AppLayout } from '@shared/theme/layout/AppLayout';
+import { Outlet } from 'react-router-dom';
 import { DashboardPage } from '@features/dashboard/pages/DashboardPage';
 
 export const DashboardRouter = () => {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<DashboardPage />} />
-      </Route>
-    </Routes>
+    <>
+      <DashboardPage />
+      <Outlet />
+    </>
   );
 };
