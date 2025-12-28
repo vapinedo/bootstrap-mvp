30eeac52f02016aae9047043646125f528d86684
Victor Pinedo <victorpinedomartinez@gmail.com>
2025-12-28 14:58:53 -0500

    Arreglar content admin theem (#14)

--- apps/frontend/src/AppRouter.jsx
+++ apps/frontend/src/AppRouter.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { NotFound } from '@shared/pages/NotFound.jsx';
+import { AppLayout } from '@shared/theme/layout/AppLayout';
 import { DashboardRouter } from '@features/dashboard/DashboardRouter';
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/dashboard/*" element={<DashboardRouter />} />
+      <Route element={<AppLayout />}>
+        <Route index path="/" element={<DashboardRouter />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
--- apps/frontend/src/shared/theme/icons/svg.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.svg" {
-  import * as React from "react";
-  const content: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default content;
-}
