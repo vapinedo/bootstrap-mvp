c16bb6bb5607bfea1e7f96cfec5c6f8634891843
Victor Pinedo <victorpinedomartinez@gmail.com>
2025-12-28 14:54:59 -0500

    arreglar dark mode

--- apps/frontend/src/features/dashboard/pages/DashboardPage.jsx
+++ apps/frontend/src/features/dashboard/pages/DashboardPage.jsx
@@ -1,26 +1,33 @@
+import { EcommerceMetrics } from '../components/ecommerce/EcommerceMetrics';
+import { MonthlySalesChart } from '../components/ecommerce/MonthlySalesChart';
+import { MonthlyTarget } from '../components/ecommerce/MonthlyTarget';
+import { StatisticsChart } from '../components/ecommerce/StatisticsChart';
+import { DemographicCard } from '../components/ecommerce/DemographicCard';
+import { RecentOrders } from '../components/ecommerce/RecentOrders';
+
 export const DashboardPage = () => {
 	return (
 		<>
 			<div className="grid grid-cols-12 gap-4 md:gap-6">
 				<div className="col-span-12 space-y-6 xl:col-span-7">
-					<p>Ecommerce Metrics</p>
-					<p>Monthly Sales Chart</p>
+					<EcommerceMetrics />
+					<MonthlySalesChart />
 				</div>
 
 				<div className="col-span-12 xl:col-span-5">
-					<p>Monthly Target</p>
+					<MonthlyTarget />
 				</div>
 
 				<div className="col-span-12">
-					<p>Statistics Chart</p>
+					<StatisticsChart />
 				</div>
 
 				<div className="col-span-12 xl:col-span-5">
-					<p>Demographic Card</p>
+					<DemographicCard />
 				</div>
 
 				<div className="col-span-12 xl:col-span-7">
-					<p>Recent Orders</p>
+					<RecentOrders />
 				</div>
 			</div>
 		</>
