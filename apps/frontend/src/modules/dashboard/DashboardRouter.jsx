4cddde92ee5d0fd1173106204ee3a4147acd6fa9
Victor Pinedo <victorpinedomartinez@gmail.com>
2025-12-28 15:45:38 -0500

    agregar index para pages en dashboard modules

--- apps/frontend/src/modules/dashboard/DashboardRouter.jsx
+++ apps/frontend/src/modules/dashboard/DashboardRouter.jsx
@@ -1,6 +1,6 @@
 
 import { Outlet } from 'react-router-dom';
-import { DashboardPage } from '@modules/dashboard/pages/DashboardPage';
+import { DashboardPage } from '@modules/dashboard/pages';
 
 export const DashboardRouter = () => {
   return (
