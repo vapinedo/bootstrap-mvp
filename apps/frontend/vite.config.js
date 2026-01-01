15185d8f0bc61a8927508bd4731d951fc9612616
Victor Pinedo <victorpinedomartinez@gmail.com>
2026-01-01 11:03:43 -0500

    Integrar tipescript (#20)

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
