936af9e7b7c536fb5d06ffaadb9c23d2aa66228f
Victor Pinedo <victorpinedomartinez@gmail.com>
2026-01-01 09:30:45 -0500

    remover items hardcodeados

--- apps/frontend/src/shared/theme/components/sidebar/SidebarMenuItems.jsx
+++ apps/frontend/src/shared/theme/components/sidebar/SidebarMenuItems.jsx
@@ -1,4 +1,4 @@
-import { GridIcon, DocsIcon, UserCircleIcon, ListIcon, TableIcon, PageIcon, PieChartIcon, BoxCubeIcon, PlugInIcon } from "@shared/theme/icons";
+import { GridIcon, DocsIcon, PieChartIcon } from "@shared/theme/icons";
 
 export const navItems = [
   {
@@ -10,30 +10,7 @@ export const navItems = [
     icon: <DocsIcon />,
     name: "Seguridad",
     subItems: [{ name: "Usuarios", path: "/user" }],
-  },
-  {
-    icon: <UserCircleIcon />,
-    name: "User Profile",
-    path: "/profile",
-  },
-  {
-    name: "Forms",
-    icon: <ListIcon />,
-    subItems: [{ name: "Form Elements", path: "/form-elements" }],
-  },
-  {
-    name: "Tables",
-    icon: <TableIcon />,
-    subItems: [{ name: "Basic Tables", path: "/basic-tables" }],
-  },
-  {
-    name: "Pages",
-    icon: <PageIcon />,
-    subItems: [
-      { name: "Blank Page", path: "/blank" },
-      { name: "404 Error", path: "/error-404" },
-    ],
-  },
+  }
 ];
 
 export const othersItems = [
@@ -44,25 +21,5 @@ export const othersItems = [
       { name: "Line Chart", path: "/line-chart" },
       { name: "Bar Chart", path: "/bar-chart" },
     ],
-  },
-  {
-    icon: <BoxCubeIcon />,
-    name: "UI Elements",
-    subItems: [
-      { name: "Alerts", path: "/alerts" },
-      { name: "Avatar", path: "/avatars" },
-      { name: "Badge", path: "/badge" },
-      { name: "Buttons", path: "/buttons" },
-      { name: "Images", path: "/images" },
-      { name: "Videos", path: "/videos" },
-    ],
-  },
-  {
-    icon: <PlugInIcon />,
-    name: "Authentication",
-    subItems: [
-      { name: "Sign In", path: "/signin" },
-      { name: "Sign Up", path: "/signup" },
-    ],
-  },
+  }
 ];
