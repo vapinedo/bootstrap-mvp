9fbfba1708bf1329c95e7a60aae07eb4847b4e18
Victor Pinedo <victorpinedomartinez@gmail.com>
2026-01-01 14:10:09 -0500

    agregar gestion loading y error

--- apps/frontend/src/modules/user/pages/UserAdminPage.tsx
+++ apps/frontend/src/modules/user/pages/UserAdminPage.tsx
@@ -5,11 +5,12 @@ export const UserAdminPage = () => {
 	const { getAllUsers } = useUser();
 	const { data, isLoading, error } = getAllUsers();
 
+	if (isLoading) return <div>Cargando ...</div>;
+	if (error) return <div>Error: {String((error as Error)?.message || error)}</div>;
+
 	return (
 		<Card>
 			<h2>Lista de Usuarios</h2>
-			{isLoading && <div>Cargando usuarios...</div>}
-			{error && <div>Error: {String((error as Error)?.message || error)}</div>}
 			<ul>
 				{data?.map((user) => (
 					<li key={user.id}>{user.username} - {user.email}</li>
